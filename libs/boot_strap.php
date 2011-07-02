c8f85c09d5a9fd7df9241e90a450c279d7ea2bda
Miftah Faridl <vcode84@gmail.com>
2011-07-02 17:47:58 +0700

    nambahi direktory sing keliru

--- libs/boot_strap.php
+++ libs/boot_strap.php
@@ -9,4 +9,4 @@
 // BASE_PATH adalah lokasi absolute path sampai ke berita21
 define('BASE_PATH', dirname( dirname(__FILE__) . '../') );
 
-include_once(BASE_PATH . '/' . 'db_config.php');
+include_once(BASE_PATH . '/' . 'libs/db_config.php');
