02a76ea78b14d02b06abe93103a76b9f7d9fd88f
Rio Astamal <me@rioastamal.net>
2011-07-03 20:48:07 +0700

    Add:  - Menambahkan comment level pada fungsi  - mengubah behaviour load_model dengan hanya menerima nama model saja tidak perlu nama lengkap file    behaviour lama:    load_model('kategori_model.php');
       behaviour baru:
       load_model('kategori');

--- libs/function.php
+++ libs/function.php
@@ -1,15 +1,26 @@
 <?php
 
 /**
- * Fungsi untuk meload model database
+ * Fungsi untuk meload model database.
+ *
+ * <code>
+ * // jika akan meload model dengan nama 'kategori_model.php' maka cara penulisannya adalah
+ * load_model('kategori');
+ * </code>
+ *
+ * @author Rio Astamal <me@rioastamal.net>
+ * @since Version 1.0
+ *
+ * @param string $model_name nama dari model yang akan diload
+ * @return void
  */
 function load_model($model_name) {
 	// load model dari base path
 	// hasilnya adalah /path/to/berita21/models/nama_model.php
-	$path_file = BASE_PATH . '/models/' . $model_name;
+	$path_file = BASE_PATH . '/models/' . $model_name . '_model.php';
 	
 	// jika file tidak ada maka model tidak bisa diload
-	if (!file_exists($path)) {
+	if (!file_exists($path_file)) {
 		// keluar dari sistem
 		exit ("Model '{$model_name}' tidak ada pada path system.");
 	}
