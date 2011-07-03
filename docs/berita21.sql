3e17d3fa797163e29800afe1e0847ab082c00bcd
Rio Astamal <me@rioastamal.net>
2011-07-03 22:42:49 +0700

    Fix:  - Menambahkan struktur auto_increment dan index pada tabel-tabel yang diperlukan
    New:
     - Menambahkan file SQL dump untuk contoh data pada contoh_data.sql

--- docs/berita21.sql
+++ docs/berita21.sql
@@ -1,153 +1,105 @@
--- MySQL dump 10.13  Distrib 5.5.8, for Linux (i686)
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: berita21
--- ------------------------------------------------------
--- Server version	5.5.8-log
+-- Host: localhost
+-- Generation Time: Jul 03, 2011 at 05:38 PM
+-- Server version: 5.5.8
+-- PHP Version: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `berita21`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `artikel`
 --
 
-DROP TABLE IF EXISTS `artikel`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `artikel` (
-  `artikel_id` int(8) unsigned NOT NULL,
+CREATE TABLE IF NOT EXISTS `artikel` (
+  `artikel_id` int(8) unsigned NOT NULL AUTO_INCREMENT,
   `artikel_judul` varchar(100) NOT NULL,
   `artikel_isi` text NOT NULL,
   `artikel_tgl` datetime NOT NULL,
   PRIMARY KEY (`artikel_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
---
--- Dumping data for table `artikel`
---
-
-LOCK TABLES `artikel` WRITE;
-/*!40000 ALTER TABLE `artikel` DISABLE KEYS */;
-/*!40000 ALTER TABLE `artikel` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `artikel_kategori`
 --
 
-DROP TABLE IF EXISTS `artikel_kategori`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `artikel_kategori` (
+CREATE TABLE IF NOT EXISTS `artikel_kategori` (
   `artikel_id` int(8) unsigned NOT NULL,
   `kategori_id` int(2) unsigned NOT NULL,
   PRIMARY KEY (`artikel_id`,`kategori_id`),
-  KEY `fk_artikel_has_kategori_kategori1` (`kategori_id`),
-  CONSTRAINT `fk_artikel_has_kategori_artikel1` FOREIGN KEY (`artikel_id`) REFERENCES `artikel` (`artikel_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_artikel_has_kategori_kategori1` FOREIGN KEY (`kategori_id`) REFERENCES `kategori` (`kategori_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `fk_artikel_has_kategori_kategori1` (`kategori_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `artikel_kategori`
---
-
-LOCK TABLES `artikel_kategori` WRITE;
-/*!40000 ALTER TABLE `artikel_kategori` DISABLE KEYS */;
-/*!40000 ALTER TABLE `artikel_kategori` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `artikel_komentar`
 --
 
-DROP TABLE IF EXISTS `artikel_komentar`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `artikel_komentar` (
+CREATE TABLE IF NOT EXISTS `artikel_komentar` (
   `artikel_id` int(8) unsigned NOT NULL,
   `komentar_id` int(8) unsigned NOT NULL,
   PRIMARY KEY (`artikel_id`,`komentar_id`),
-  KEY `fk_artikel_has_komentar_komentar1` (`komentar_id`),
-  CONSTRAINT `fk_artikel_has_komentar_artikel` FOREIGN KEY (`artikel_id`) REFERENCES `artikel` (`artikel_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_artikel_has_komentar_komentar1` FOREIGN KEY (`komentar_id`) REFERENCES `komentar` (`komentar_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `fk_artikel_has_komentar_komentar1` (`komentar_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `artikel_komentar`
---
-
-LOCK TABLES `artikel_komentar` WRITE;
-/*!40000 ALTER TABLE `artikel_komentar` DISABLE KEYS */;
-/*!40000 ALTER TABLE `artikel_komentar` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `kategori`
 --
 
-DROP TABLE IF EXISTS `kategori`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `kategori` (
-  `kategori_id` int(2) unsigned NOT NULL,
+CREATE TABLE IF NOT EXISTS `kategori` (
+  `kategori_id` int(2) unsigned NOT NULL AUTO_INCREMENT,
   `kategori_nama` varchar(30) NOT NULL,
   PRIMARY KEY (`kategori_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
---
--- Dumping data for table `kategori`
---
-
-LOCK TABLES `kategori` WRITE;
-/*!40000 ALTER TABLE `kategori` DISABLE KEYS */;
-/*!40000 ALTER TABLE `kategori` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `komentar`
 --
 
-DROP TABLE IF EXISTS `komentar`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `komentar` (
-  `komentar_id` int(8) unsigned NOT NULL,
+CREATE TABLE IF NOT EXISTS `komentar` (
+  `komentar_id` int(8) unsigned NOT NULL AUTO_INCREMENT,
   `komentar_nama` varchar(25) NOT NULL,
   `komentar_email` varchar(50) NOT NULL,
   `komentar_isi` text NOT NULL,
   `komentar_tgl` datetime NOT NULL,
   PRIMARY KEY (`komentar_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `komentar`
+-- Constraints for dumped tables
 --
 
-LOCK TABLES `komentar` WRITE;
-/*!40000 ALTER TABLE `komentar` DISABLE KEYS */;
-/*!40000 ALTER TABLE `komentar` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Constraints for table `artikel_kategori`
+--
+ALTER TABLE `artikel_kategori`
+  ADD CONSTRAINT `fk_artikel_has_kategori_artikel1` FOREIGN KEY (`artikel_id`) REFERENCES `artikel` (`artikel_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_artikel_has_kategori_kategori1` FOREIGN KEY (`kategori_id`) REFERENCES `kategori` (`kategori_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2011-07-02 16:43:21
+--
+-- Constraints for table `artikel_komentar`
+--
+ALTER TABLE `artikel_komentar`
+  ADD CONSTRAINT `fk_artikel_has_komentar_artikel` FOREIGN KEY (`artikel_id`) REFERENCES `artikel` (`artikel_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_artikel_has_komentar_komentar1` FOREIGN KEY (`komentar_id`) REFERENCES `komentar` (`komentar_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
