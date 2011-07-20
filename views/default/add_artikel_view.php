f3fed0b850efd4d4f50d27dee696ed87e742f664
Miftah Faridl <vcode84@gmail.com>
2011-07-20 20:04:20 +0700

    mengedit file add_artikel_view

--- views/default/add_artikel_view.php
+++ views/default/add_artikel_view.php
@@ -4,10 +4,19 @@
 	<!-- BEGIN CONTENT -->
 	<div id="content">
 		<h1>Tambah Artikel</h1>
-		<form action="" method="post">
-			<label>Nama Artikel</label>
-			<input type="text" name="nama_kat" value="" id="" /><br/>
-			<input type="submit" name="submit-art" value="SIMPAN" />
+		<form action="test_view_artikel.php" method="post">
+			<label>Judul Artikel</label><br/>
+			<input type="text" name="judul_art" value="" size="15" /><br/>
+			<label>Isi Artikel</label><br/>
+			<textarea name="isi_art"></textarea><br/>
+			<label>Tanggal Artikel</label><br/>
+			<input type="text" name="tgl_art" value="" size="15" /> <span>pake format YYYY-MM-DD</span><br/>
+			<select name="kategori_id">
+				<?php foreach ($data['daftar_kategori'] as $kat) : ?>
+				<option value="<?php echo ($kat->kategori_id);?>"><?php echo ($kat->kategori_nama);?></option>
+				<?php endforeach; ?>
+			</select><br/>
+			<input type="submit" name="submit_artikel" value="SIMPAN" />
 		</form>
 		
 		<h3>Daftar Artikel Saat Ini</h3>
