4a45032e5ed4e0e5e7976b9499032c65885c96ae
Alfa Radito <qwertqwe16@yahoo.co.id>
2011-08-02 16:00:57 +0700

    menambahkan secure url

--- plugins/secure_url/secure_url.php
+++ plugins/secure_url/secure_url.php
@@ -2,9 +2,23 @@
 
 add_hook('pre_routing', 'secure_url');
 
+/**
+ * Fungsi untuk men-secure dari URL Attack
+ *
+ * @author Alfa Radito <qwertqwe16@yahoo.co.id>
+ * @since Version 1.0
+ *
+ * @return void
+ */
 function secure_url() {
 	// lakukan filter URL, jika terdapat karakter yang tidak diinginkan
 	// langsung keluar dari script (berhenti total)
 	
 	// logika.......
+	$url = $_SERVER['REQUEST_URI'];
+	$ret = preg_match('@^[a-zA-Z0-9:\-\._\/#]+$@', $url);
+	
+	if (!$ret) {
+		throw new Exception ("Hayo.. ngapain");
+	}
 }
