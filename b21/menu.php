18ed3d2ab32990d682d094bf703f6b1e063ba087
Alfa Radito <qwertqwe16@yahoo.co.id>
2011-08-03 19:37:45 +0700

    ADD : menambah menu

--- b21/menu.php
+++ b21/menu.php
@@ -12,8 +12,24 @@ tpl_add_menu(array(
 tpl_add_menu(array(
 	'label' => 'Add Kategori',
 	'id' => 'add_kategori',
-	'url' => get_site_url() . '/add-kategori',
+	'url' => get_site_url() . '/admin/add-kategori',
 	'title' => 'Tambah Kategori Baru',
+	'order' => 1
+));	
+
+tpl_add_menu(array(
+	'label' => 'Add Artikel',
+	'id' => 'add_artikel',
+	'url' => get_site_url() . '/admin/add-artikel',
+	'title' => 'Tambah Artikel Baru',
+	'order' => 1
+));	
+
+tpl_add_menu(array(
+	'label' => 'View Artikel',
+	'id' => 'view_artikel',
+	'url' => get_site_url() . '/admin/view-artikel',
+	'title' => 'Lihat Artikel',
 	'order' => 1
 ));	
 
