3d866efdf5a0ed2a655d3da8399ad9b5801662e1
Rio Astamal <me@rioastamal.net>
2011-08-04 18:48:51 +0700

    Menambahkan beberapa file atau direktori untuk pengetesan fungsi-fungsi load_library load_model dan load_view pada plugin

--- plugins/hello_dolly/hello_dolly.php
+++ plugins/hello_dolly/hello_dolly.php
@@ -16,3 +16,12 @@ add_hook('page_head', 'heldo_head');
 function heldo_head() {
 	echo ("<!-- I'm Hello Dolly ;) -->\n");
 }
+
+// TEST LOAD VIEW 
+// load_view('dolly_test_view');
+
+// TEST LOAD MODEL
+// load_model('dolly_model');
+
+// TEST LOAD LIBRARY
+// load_library('dolly_test');
