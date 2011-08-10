65c2df35528fa4dffe0451815d94f51e25ce0c05
Alfa Radito <qwertqwe16@yahoo.co.id>
2011-08-10 18:27:31 +0700

    commit helper

---
+++ helpers/url_helper.php
@@ -0,0 +1,21 @@
+<?php
+
+function get_argument_by($key) {
+	global $_MR;
+	if (in_array($key, $_MR['controller_arguments'])) {
+		$a = array_search($key, $_MR['controller_arguments']);
+		if ($a == 0) {
+			$a += 1;
+			print_r($_MR['controller_arguments'][$a]);
+		} elseif ($a % 2 == 0) {
+			$a += 1;
+			print_r($_MR['controller_arguments'][$a]);
+		} else {
+			return FALSE;
+		}
+		
+	}
+	
+}
+
+?>
