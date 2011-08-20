34b99d5f8bf10aff7c8f0a7043ca8e2223718274
Rio Astamal <me@rioastamal.net>
2011-08-20 11:59:57 +0700

    Menambahkan banyak hooks baru pada boot strap file

--- mr/boot_strap.php
+++ mr/boot_strap.php
@@ -44,6 +44,11 @@ site_debug(FRAMEWORK_FULL_NAME, 'FRAMEWORK NAME');
 
 load_helper('url');
 load_helper('string');
+
+// load models
+load_model('users');
+load_model('options');
+
 $_mr_auto_libs = array(
 					'query_cache',
 					'sql_query',
@@ -57,10 +62,10 @@ $_MR['autoload_libraries'] = $_mr_auto_libs + $_MR['autoload_libraries'];
 foreach ($_MR['autoload_libraries'] as $lib) {
 	load_library($lib);
 }
+run_hooks('boot_post_libraries_load');
 
-// load models
-load_model('users');
-load_model('options');
+// letakkan hooks sebelum proses koneksi ke database
+run_hooks('boot_pre_db_conn');
 
 // konek ke database
 $_MR['db'] = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
@@ -70,20 +75,27 @@ if (mysqli_connect_error()) {
 	exit('DB_ERROR(' . mysqli_connect_errno() . '): ' . mysqli_connect_error());
 }
 
+// hooks setelah koneksi db
+run_hooks('boot_post_db_conn');
+
 // load menu
 include_once(BASE_PATH . '/mr/' . 'menu.php');
 
-run_hooks('mr_init');
-
+run_hooks('boot_pre_session_init');
 // init session
 mr_session_construct();
+run_hooks('boot_post_session_init');
 
 // set all options from database to global variabel
 set_all_options();
+run_hooks('boot_post_options_load');
 
 // load plugins
 load_plugins();
+run_hooks('boot_post_plugins_load');
 
 // init user role
+run_hooks('boot_pre_user_role');
 init_user_role();
+run_hooks('boot_post_user_role');
 site_debug(print_r($_MR['user'], TRUE), 'CURRENT USER');
